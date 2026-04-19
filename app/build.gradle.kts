d8f3bb5905ebbbf4b76122faf7608bc2dcfe3a8c
kshmrrZy <dimonmdimonr@gmail.com>
2026-04-20 01:10:45 +0400

    Deduplicate coordinatorlayout artifact in app dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,10 +39,9 @@ android {
     }
 }
 
-configurations.all {
-    resolutionStrategy {
-        force("androidx.annotation:annotation-experimental:1.4.1")
-    }
+configurations.configureEach {
+    exclude(group = "androidx.annotation", module = "annotation-experimental")
+    exclude(group = "androidx.coordinatorlayout", module = "coordinatorlayout")
 }
 
 dependencies {
@@ -53,9 +52,10 @@ dependencies {
 
     implementation("org.videolan.android:libvlc-all:3.6.0") {
         exclude(group = "androidx.annotation", module = "annotation-experimental")
+        exclude(group = "androidx.coordinatorlayout", module = "coordinatorlayout")
     }
-
     implementation("androidx.annotation:annotation-experimental:1.4.1")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
